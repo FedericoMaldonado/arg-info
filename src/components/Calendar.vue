926453eb76e2046612df8f018143b4c2e6c1d5fe
FedericoMaldonado <fedework01@gmail.com>
2024-05-13 17:36:30 -0300

    calendario agregado

---
+++ src/components/Calendar.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+
+<template>
+  <VCalendar is-dark="system" />
+</template>
+
+<style scoped>
+.v-col {
+  color: yellow;
+}
+.v-sheet {
+  color: white;
+  background-color: #212121;
+}
+</style>
