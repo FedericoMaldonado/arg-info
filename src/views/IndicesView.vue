c8ddd6fe54cd3659381821b9744744cfb211a1d2
FedericoMaldonado <fedework01@gmail.com>
2024-05-28 11:44:52 -0300

    graficos de los indices importados a la vista

--- src/views/IndicesView.vue
+++ src/views/IndicesView.vue
@@ -1,10 +1,51 @@
 <template>
   <Toolbar />
-  <h1>Indices</h1>
+  <h1>Índices</h1>
+  <div class="container-graficos">
+    <div class="graficos">
+      <GraficoInflacionMensual />
+      <GraficoInflacionInteranual />
+    </div>
+  </div>
+  <div class="graficoUVA">
+    <GraficoUVA />
+  </div>
 </template>
 
 <script setup lang="ts">
 import Toolbar from "@/components/Toolbar.vue";
+import GraficoInflacionMensual from "@/components/GraficoInflacionMensual.vue";
+import GraficoInflacionInteranual from "@/components/GraficoInflacionInteranual.vue";
+import GraficoUVA from "@/components/GraficoUVA.vue";
 </script>
 
-<style scoped></style>
+<style scoped>
+h1 {
+  padding-top: 50px;
+}
+.container-graficos {
+  display: flex;
+  justify-content: center;
+  margin: 10px;
+}
+
+.graficos {
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  width: 100%;
+  gap: 5px;
+}
+
+.graficoUVA {
+  display: flex;
+  justify-content: center;
+  margin: 10px;
+}
+
+@media (max-width: 615px) {
+  .graficos {
+    display: grid;
+    grid-template-columns: repeat(1, 1fr);
+  }
+}
+</style>
