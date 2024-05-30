13982f0151f459d87d95e69e88baf3f1b9a88a5d
FedericoMaldonado <fedework01@gmail.com>
2024-05-30 17:55:53 -0300

    incorporacion de los graficos terminada

--- src/views/TasasView.vue
+++ src/views/TasasView.vue
@@ -1,10 +1,40 @@
 <template>
   <Toolbar />
   <h1>Tasas</h1>
+  <div class="container-graficos">
+    <div class="graficos">
+      <GraficoPlazoFijo />
+      <GraficoDepositosA30Dias />
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts">
 import Toolbar from "@/components/Toolbar.vue";
+import GraficoPlazoFijo from "@/components/GraficoPlazoFijo.vue";
+import GraficoDepositosA30Dias from "@/components/GraficoDepositosA30Dias.vue";
 </script>
 
-<style scoped></style>
+<style scoped>
+h1 {
+  padding-top: 50px;
+}
+.container-graficos {
+  display: flex;
+  margin: 10px;
+}
+
+.graficos {
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  gap: 5px;
+}
+
+@media (max-width: 615px) {
+  .graficos {
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+}
+</style>
