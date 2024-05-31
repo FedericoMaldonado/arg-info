0f414dad1dfb59b882ad61303e98748de9a87bf6
FedericoMaldonado <fedework01@gmail.com>
2024-05-31 14:04:41 -0300

    vista de rendimientos terminada (Rendimientos.View)

--- src/views/RendimientosView.vue
+++ src/views/RendimientosView.vue
@@ -1,10 +1,94 @@
 <template>
   <Toolbar />
   <h1>Rendimientos</h1>
+  <article class="container-principal">
+    <v-select
+      label="Seleccionar moneda"
+      :items="['buenbit', 'fiwind', 'letsbit', 'belo', 'lemoncash', 'ripio']"
+      v-model="selectedOption"
+      variant="outlined"
+      color="#e6ae4a"
+    ></v-select>
+
+    <v-container>
+      <section
+        class="container-monedas"
+        v-for="moneda in filteredMoney"
+        :key="moneda"
+      >
+        <v-table class="tabla">
+          <thead>
+            <tr>
+              <th class="text-center">Moneda</th>
+              <th class="text-center">Apy</th>
+              <th class="text-center">Fecha</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="value in moneda.rendimientos" :key="value">
+              <td class="text-center">{{ value.moneda }}</td>
+              <td class="text-center">{{ value.apy }}</td>
+              <td class="text-center">{{ value.fecha }}</td>
+            </tr>
+          </tbody>
+        </v-table>
+      </section>
+    </v-container>
+  </article>
 </template>
 
 <script setup lang="ts">
 import Toolbar from "../components/Toolbar.vue";
+import { ref, computed } from "vue";
+import { useGetData } from "@/composables/useGetData";
+
+const { getData, data } = useGetData();
+
+const selectedOption = ref("buenbit");
+
+//Filtro los nombres de las monedas en las opciones del v-select
+const filteredMoney = computed(() => {
+  if (data.value && Array.isArray(data.value)) {
+    return data.value.filter((moneda: any) => {
+      return moneda.entidad === selectedOption.value;
+    });
+  }
+  return [];
+});
+
+getData(" https://api.argentinadatos.com/v1/finanzas/rendimientos");
+console.log(data);
 </script>
 
-<style scoped></style>
+<style scoped>
+h1 {
+  padding-top: 50px;
+}
+
+.container-monedas {
+  display: flex;
+  flex-direction: column;
+}
+.tabla {
+  border: 1px solid #1976d2;
+  background-color: #212121;
+  border-radius: 4px;
+}
+
+.tabla th {
+  color: #e6ae4a;
+}
+
+.tabla td {
+  color: #ffffff;
+}
+
+thead {
+  font-size: 20px;
+}
+
+tbody td,
+thead th {
+  border: 1px solid #1976d2;
+}
+</style>
