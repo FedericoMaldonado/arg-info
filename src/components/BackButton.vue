736283c157c0b072ce47c23511bcdd87bf08b04b
FedericoMaldonado <fedework01@gmail.com>
2024-06-04 20:50:02 -0300

    componente de boton volver creado

---
+++ src/components/BackButton.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-btn variant="outlined" @click="back" density="comfortable"> volver </v-btn>
+</template>
+
+<script setup lang="ts">
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+
+const back = () => {
+  router.push("/fci");
+};
+</script>
+
+<style scoped>
+.v-btn:hover {
+  border-color: #e6ae4a;
+}
+</style>
